f73bb8b29f7042bd60b5f998b37382d362051156
anjmo4568 <anjmo4568@gmail.com>
2026-09-09 22:02:47 +0900

    chore: apps-in-toss SDK 3.x 업그레이드

---
+++ apps-in-toss.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from '@apps-in-toss/web-framework/config'
+
+export default defineConfig({
+  appName: 'manga-quiz',
+
+  brand: {
+    primaryColor: '#D85A30'
+  },
+
+  permissions: [],
+  webBundleDir: 'dist'
+})
